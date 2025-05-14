81972ace88e9e825bb9d5e54491edccd8de346d3
Antriksh Prasad <antrikshprasad@Antrikshs-MacBook-Air.local>
2025-05-14 20:40:45 +0530

    feat: lucky 7 game

--- src/LuckyN.jsx
+++ src/LuckyN.jsx
@@ -4,7 +4,12 @@ import Button from "./Button";
 import Dice from "./Dice";
 
 const LuckyN = ({ title = "Dice Game", numDice = 2, winCheck }) => {
+  
   const [dice, setDice] = useState(getRolls(numDice));
+  /*
+    You're calling the function that was passed as a prop —
+    and you're passing the dice array (from state) as the argument.
+  */
   const isWinner = winCheck(dice);
   const roll = () => setDice(getRolls(numDice));
 
